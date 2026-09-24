88001cb52c1a53eb6a504ddfe60af826df77471e
John <jpbadillo05@gmail.com>
2026-09-24 13:01:36 -0400

    Test MetricLog types and document ADT

--- metric_log.cpp
+++ metric_log.cpp
@@ -14,12 +14,19 @@ using namespace std;
  *
  * Data:
  * [Complete in Part E: describe the values managed by this log.]
+ * A fixed-capacity collection of elements of generic type T (up to CAPACITY elements)
+ * and an integer representing the current number of stored elements.
  *
  * Operations:
  * [Complete in Part E: describe add(value).]
+ * Adds a generic element to the log if space is available; returns true if successful, false if full.
  * [Complete in Part E: describe get(index) and its precondition.]
+ * Retrieves the stored element at the given index. Precondition: 0 <= index < size().
  * [Complete in Part E: describe contains(target).]
+ * Searches for a specific value in the log; returns true if found, false otherwise.
  * [Complete in Part E: describe size() and isEmpty().]
+ * size(): Returns the current number of elements stored in the log.
+ * isEmpty(): Returns true if the log contains no elements, false otherwise.
  */
 
 template <typename T>
@@ -84,6 +91,19 @@ public:
 // It receives a generic target by const reference.
 // Return true when an equal stored value exists; otherwise return false.
 
+template <typename T>
+bool MetricLog<T>::contains(const T& target) const
+{
+    for (int i = 0; i < count; i++)
+    {
+        if (values[i] == target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
     MetricLog<int> waitlistCounts;
@@ -102,6 +122,15 @@ int main()
     // TODO (Part E): Add two dummy duration values to that log.
     // TODO (Part E): Use contains with one value that exists and one that does not exist.
     // TODO (Part E): Print descriptive English labels for all results.
+
+    MetricLog<double> sessionDurations;
+
+    sessionDurations.add(12.5);
+    sessionDurations.add(18.3);
+
+    cout << "Session durations: " << sessionDurations.size() << endl;
+    cout << "Contains 12.5: " << (sessionDurations.contains(12.5) ? "True" : "False") << endl;
+    cout << "Contains 15.0: " << (sessionDurations.contains(15.0) ? "True" : "False") << endl;
 
     return 0;
 }
